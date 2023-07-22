61a8a3f103d0ef6d43086e3bd9a4a645037ee670
Andy Miao Gu <andymiaogu@Andys-MacBook-Air.local>
2023-07-21 20:24:49 -0700

    message

--- Web development/flask and tailwindcss/main.py
+++ Web development/flask and tailwindcss/main.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route("/andy")
+def hello_world2():
+    return render_template("andy.html")
+
+@app.route("/home")
+def hello_world3():
+    return render_template("home.html")
+@app.route("/calculator")
+def hello_world4():
+    return render_template("calculator.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
