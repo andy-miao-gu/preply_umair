86ef3be4b3b5a36ae10e3d9a77593cd5a32c7ab8
Andy Miao Gu <andymiaogu@Andys-MacBook-Air.local>
2023-07-24 18:58:12 -0700

    hiiii

--- Web development/flask and tailwindcss/main.py
+++ Web development/flask and tailwindcss/main.py
@@ -17,12 +17,32 @@ def hello_world3():
 def hello_world4():
     return render_template("calculator.html")
 
+
+
+data_list = []  # A list to store the data (name, ID, and age)
+
+@app.route('/form', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        user_id = request.form['id']
+        age = request.form['age']
+        data_list.append({'name': name, 'id': user_id, 'age': age})
+    return render_template('form.html', data_list=data_list)
+
+
+
+
 # a function for GET POST requests
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
     data = request.get_json()
     expression = data['expression']
+
+@app.route('/hipage')
+def hiiiii():
+    return render_template("hiiiii.html")
 
     try:
         # Evaluate the expression to get the result
